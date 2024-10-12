74f855e352f33a98588ca28fd7ebd0ba117faaaf
unknown <mohammedfedaily@gmail.com>
2024-10-12 20:14:40 +0200

    Update style, responsive

--- src/app/pages/auth/reset-password/page.tsx
+++ src/app/pages/auth/reset-password/page.tsx
@@ -8,31 +8,9 @@ const ResetPasswordPage = () => {
   return (
     <div>
       <Header />
-      <Hero title="Reset Password" image="/reset-password.jpg" />
-<<<<<<< HEAD
       <Suspense fallback={<div>Loading...</div>}>
         <ResetPassword />
       </Suspense>
-=======
-      <div className="container mx-auto py-28 p-8">
-        <div className="max-w-lg mx-auto bg-white p-8 rounded-lg shadow-lg">
-          <h2 className="text-2xl font-bold mb-6 text-center text-primary">
-            Set New Password
-          </h2>
-          {token ? (
-            <Form
-              fields={fields}
-              onSubmit={handleSubmit}
-              submitButtonText="Reset Password"
-            />
-          ) : (
-            <p className="text-center text-red-500">
-              Invalid or missing reset token.
-            </p>
-          )}
-        </div>
-      </div>
->>>>>>> ed8981dd863ade234fbfcb4e351cebc4fc49b0e8
       <Footer />
     </div>
   );
--- src/app/pages/searchResult/page.tsx
+++ src/app/pages/searchResult/page.tsx
@@ -9,14 +9,9 @@ const SearchResultsPage: React.FC = () => {
   return (
     <div>
       <Header />
-<<<<<<< HEAD
-      <Hero title="Search Results" image="/hero-image.jpg" />
       <Suspense fallback={<div>Loading...</div>}>
         <SearchResults />
       </Suspense>
-=======
-      <SearchResults />
->>>>>>> ed8981dd863ade234fbfcb4e351cebc4fc49b0e8
       <Footer />
     </div>
   );
